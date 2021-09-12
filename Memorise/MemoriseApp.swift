c37827ab0c4b2194781e76bc9d9114fe661d4f48
Jared Swift <=>
2021-09-12 20:43:18 +0100

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Jared William Swift on 12/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
